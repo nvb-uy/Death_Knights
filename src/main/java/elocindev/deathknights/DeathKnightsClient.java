4daf92625f6d16fa410a6127035c1e4b51180135
nvb-uy <elocindev@lunapixel.gg>
2024-10-09 11:15:51 -0300

    add(changes to death coil model), bump(spell engine)

--- src/main/java/elocindev/deathknights/DeathKnightsClient.java
+++ src/main/java/elocindev/deathknights/DeathKnightsClient.java
@@ -1,11 +1,19 @@
 package elocindev.deathknights;
 
 import net.fabricmc.api.ClientModInitializer;
-
+import net.minecraft.util.Identifier;
+import net.spell_engine.api.render.CustomModels;
 import elocindev.deathknights.config.ConfigLoader;
+
+import java.util.List;
+
 public class DeathKnightsClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		ConfigLoader.initClient();
+
+		CustomModels.registerModelIds(List.of(
+			new Identifier(DeathKnights.MODID, "projectile/unholy_missile")
+		));
 	}
 }
