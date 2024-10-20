c67c3263b3e1106ade29431994495f982e2c6f8c
nvb-uy <elocindev@lunapixel.gg>
2024-10-20 19:51:48 -0300

    feat(blood particles)

--- src/main/java/elocindev/deathknights/registry/ParticleRegistry.java
+++ src/main/java/elocindev/deathknights/registry/ParticleRegistry.java
@@ -10,10 +10,14 @@ import net.minecraft.util.Identifier;
 public class ParticleRegistry {
     public static final DefaultParticleType UNHOLY_HIT = FabricParticleTypes.simple();
     public static final DefaultParticleType UNHOLY_SMOKE = FabricParticleTypes.simple();
+    public static final DefaultParticleType BLOOD_HIT = FabricParticleTypes.simple();
+    public static final DefaultParticleType BLOOD_BOIL = FabricParticleTypes.simple();
 
 
     public static void register() {
         Registry.register(Registries.PARTICLE_TYPE, new Identifier(DeathKnights.MODID, "unholy_hit"), UNHOLY_HIT);
         Registry.register(Registries.PARTICLE_TYPE, new Identifier(DeathKnights.MODID, "unholy_smoke"), UNHOLY_SMOKE);
+        Registry.register(Registries.PARTICLE_TYPE, new Identifier(DeathKnights.MODID, "blood_hit"), BLOOD_HIT);
+        Registry.register(Registries.PARTICLE_TYPE, new Identifier(DeathKnights.MODID, "blood_boil"), BLOOD_BOIL);
     }
 }
