cdbf69e0f457713728b03539fdbb2b1c8fe9fd0c
nvb-uy <elocindev@lunapixel.gg>
2024-10-22 19:06:17 -0300

    fix(taunt)

--- src/main/java/elocindev/deathknights/spells/blood/DarkCommandHandler.java
+++ src/main/java/elocindev/deathknights/spells/blood/DarkCommandHandler.java
@@ -4,6 +4,7 @@ import net.spell_engine.api.event.CombatEvents;
 import elocindev.deathknights.registry.SpellRegistry;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.player.PlayerEntity;
 
 public class DarkCommandHandler {
@@ -16,8 +17,10 @@ public class DarkCommandHandler {
                 if (args.spell().id().toString().equals("death_knights:dark_command")) {
                     for (var target : args.targets()) {
                         if (target instanceof LivingEntity ent) {
-                            ent.tryAttack(caster);
+                            ent.setAttacking(caster);
                             ent.addStatusEffect(new StatusEffectInstance(SpellRegistry.ENRAGED, 100, 0));
+                        } if (target instanceof MobEntity mob) {
+                            mob.setTarget(caster);
                         }
                     }
                 }
