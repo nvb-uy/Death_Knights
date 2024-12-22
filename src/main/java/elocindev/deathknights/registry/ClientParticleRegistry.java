b5eb20f55255988ec4f2e847906843a7a024bda0
nvb-uy <elocindev@lunapixel.gg>
2024-12-22 14:57:50 -0300

    fix(client particle)

--- src/main/java/elocindev/deathknights/registry/ClientParticleRegistry.java
+++ src/main/java/elocindev/deathknights/registry/ClientParticleRegistry.java
@@ -1,15 +1,19 @@
 package elocindev.deathknights.registry;
 
-import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
+//? if 1.20.1 {
+/*import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
 import net.spell_engine.client.particle.SpellFlameParticle;
+*///?}
 
 public class ClientParticleRegistry {
     public static void register() {
-        var instance = ParticleFactoryRegistry.getInstance();
+        //? if 1.20.1 {
+        /*var instance = ParticleFactoryRegistry.getInstance();
 
         instance.register(ParticleRegistry.UNHOLY_HIT, SpellFlameParticle.FlameFactory::new);
         instance.register(ParticleRegistry.UNHOLY_SMOKE, SpellFlameParticle.MediumFlameFactory::new);
         instance.register(ParticleRegistry.BLOOD_HIT, SpellFlameParticle.FlameFactory::new);
         instance.register(ParticleRegistry.BLOOD_BOIL, SpellFlameParticle.MediumFlameFactory::new);     
+        *///?} else {
     }
 }
