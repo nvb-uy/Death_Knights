5735428b432cc2cbf9e177224b04fa298164eed3
nvb-uy <elocindev@lunapixel.gg>
2024-12-22 15:01:49 -0300

    fix(loot in 1.21)

--- src/main/java/elocindev/deathknights/loot/LootInjector.java
+++ src/main/java/elocindev/deathknights/loot/LootInjector.java
@@ -2,21 +2,29 @@ package elocindev.deathknights.loot;
 
 import elocindev.deathknights.config.Configs;
 import elocindev.deathknights.config.entries.loot.JewelryLootConfig;
-import elocindev.deathknights.registry.ItemRegistry;
 import elocindev.necronomicon.api.ResourceIdentifier;
-import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+
 import net.minecraft.item.Item;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.LootTable;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.provider.number.BinomialLootNumberProvider;
-import net.minecraft.util.Identifier; import elocindev.necronomicon.api.ResourceIdentifier;
+import net.minecraft.util.Identifier;
+
+import elocindev.deathknights.registry.ItemRegistry;
+
+//? if 1.20.1 {
+/*import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+*///?} else {
+import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
+//?}
 
 public class LootInjector {
     public static JewelryLootConfig CONFIG = Configs.Loot.JEWELRY;
     
     public static void init() {
-        LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+        //? if 1.20.1 {
+        /*LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
             injectLoot(ItemRegistry.FROST_DK_NECKLACE.item(), CONFIG.frost_necklace, id, tableBuilder);
             injectLoot(ItemRegistry.FROST_DK_RING.item(), CONFIG.frost_ring, id, tableBuilder);
             injectLoot(ItemRegistry.UNHOLY_DK_NECKLACE.item(), CONFIG.unholy_necklace, id, tableBuilder);
@@ -24,6 +32,16 @@ public class LootInjector {
             injectLoot(ItemRegistry.BLOOD_DK_NECKLACE.item(), CONFIG.blood_necklace, id, tableBuilder);
             injectLoot(ItemRegistry.BLOOD_DK_RING.item(), CONFIG.blood_ring, id, tableBuilder);
         });
+        *///?} else {
+            LootTableEvents.MODIFY.register((key, tableBuilder, source, registries) -> {
+                injectLoot(ItemRegistry.FROST_DK_NECKLACE.item(), CONFIG.frost_necklace, key.getValue(), tableBuilder);
+                injectLoot(ItemRegistry.FROST_DK_RING.item(), CONFIG.frost_ring, key.getValue(), tableBuilder);
+                injectLoot(ItemRegistry.UNHOLY_DK_NECKLACE.item(), CONFIG.unholy_necklace, key.getValue(), tableBuilder);
+                injectLoot(ItemRegistry.UNHOLY_DK_RING.item(), CONFIG.unholy_ring, key.getValue(), tableBuilder);
+                injectLoot(ItemRegistry.BLOOD_DK_NECKLACE.item(), CONFIG.blood_necklace, key.getValue(), tableBuilder);
+                injectLoot(ItemRegistry.BLOOD_DK_RING.item(), CONFIG.blood_ring, key.getValue(), tableBuilder);
+            });
+        //?}
     }
 
     private static void injectLoot(Item item, JewelryLootConfig.LootHolder lootHolder, Identifier id, LootTable.Builder tableBuilder) {
